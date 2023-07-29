4bfdf6cfb435b02cf84c55894b33af620f808ac3
Jose Dev <josefernando0307@gmail.com>
2023-07-29 14:36:30 -0500

    creacion estructura del proyecto

---
+++ src/test/java/com/ecommerceenna/springecommerce/SpringEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerceenna.springecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
