677f04600bb7fcbc682f5a92a5fb5d2eeba2d570
Jose Dev <josefernando0307@gmail.com>
2023-07-30 15:02:39 -0500

    implementacion de update y delete

--- src/main/java/com/ecommerceenna/springecommerce/controler/ProductoControler.java
+++ src/main/java/com/ecommerceenna/springecommerce/controler/ProductoControler.java
@@ -10,8 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/productos")
@@ -43,4 +46,28 @@ public class ProductoControler {
         productoService.save(producto);
         return "redirect:/productos";
     }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model){
+        Producto producto = new Producto();
+        Optional<Producto> optionalProducto = productoService.get(id);
+        producto = optionalProducto.get();
+
+        LOGGER.info("producto buscado: {}", producto);
+        model.addAttribute("producto", producto);
+        return "productos/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Producto producto){
+        productoService.update(producto);
+        return "redirect:/productos";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id){
+        productoService.delete(id);
+        return "redirect:/productos";
+    }
 }
+
