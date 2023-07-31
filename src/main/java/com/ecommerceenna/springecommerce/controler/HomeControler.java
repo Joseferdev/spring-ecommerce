cc647e9e824759475360050885f42ff0c2586da9
Jose Dev <josefernando0307@gmail.com>
2023-07-31 16:32:22 -0500

    Mostrar informacion en la pagina productohome

--- src/main/java/com/ecommerceenna/springecommerce/controler/HomeControler.java
+++ src/main/java/com/ecommerceenna/springecommerce/controler/HomeControler.java
@@ -1,15 +1,23 @@
 package com.ecommerceenna.springecommerce.controler;
 
+import com.ecommerceenna.springecommerce.model.Producto;
 import com.ecommerceenna.springecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
 public class HomeControler {
+
+    private final Logger log = LoggerFactory.getLogger(HomeControler.class);
 
     @Autowired
     private ProductoService productoService;
@@ -19,4 +27,16 @@ public class HomeControler {
         model.addAttribute("productos", productoService.findAll());
         return "usuario/home";
     }
+
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id, Model model){
+        log.info("Id producto enviado como parametro {}", id);
+        Producto producto = new Producto();
+        Optional<Producto> productoOptional = productoService.get(id);
+        producto = productoOptional.get();
+
+        model.addAttribute("producto", producto);
+        return "usuario/productohome";
+    }
 }
+
