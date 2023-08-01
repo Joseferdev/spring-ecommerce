38684ccc9debfe9ecdfa0c6579cc868b48a7a52e
Jose Dev <josefernando0307@gmail.com>
2023-08-01 10:12:58 -0500

    validacion para doble producto y ver el carrito desde home

--- src/main/java/com/ecommerceenna/springecommerce/controler/HomeControler.java
+++ src/main/java/com/ecommerceenna/springecommerce/controler/HomeControler.java
@@ -64,7 +64,13 @@ public class HomeControler {
         detalleOrden.setTotal(producto.getPrecio()*cantidad);
         detalleOrden.setProducto(producto);
 
-        detalles.add(detalleOrden);
+        //validar que el producto no se añada 2 veces
+        Integer idProducto = producto.getId();
+        boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId() == idProducto);
+
+        if (!ingresado){
+            detalles.add(detalleOrden);
+        }
 
         sumaTotal = detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
 
@@ -94,6 +100,15 @@ public class HomeControler {
         sumaTotal = detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
 
         orden.setTotal(sumaTotal);
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
+
+        return "usuario/carrito";
+    }
+
+    @GetMapping("/getCart")
+    public String getCart(Model model){
+
         model.addAttribute("cart", detalles);
         model.addAttribute("orden", orden);
 
